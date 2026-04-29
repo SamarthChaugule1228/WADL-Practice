b796a90a22acaea182d130c395eb44ec3db43828
SamarthChaugule1228 <chaugulesamarth12@gmail.com>
2026-04-29 19:17:00 +0530

    NOde Employee Completed + Done

---
+++ Q17_NodeEmployee/server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const fs = require("fs")
+
+const app = express()
+
+app.use(express.static('public'));
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+//api 
+app.get("/api/employee", (req, res) => {
+    fs.readFile("employee.json", 'utf-8', (err, data) => {
+        
+        if (err)
+        {
+         return  res.status(500).json({ error: "Error Found " });
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server is listening");
+});
