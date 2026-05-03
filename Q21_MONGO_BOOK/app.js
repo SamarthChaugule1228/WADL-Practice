77779636817c1e579041a9d802710acf187c200f
SamarthChaugule1228 <chaugulesamarth12@gmail.com>
2026-05-03 23:07:12 +0530

    cors Added + Final

--- Q21_MONGO_BOOK/app.js
+++ Q21_MONGO_BOOK/app.js
@@ -1,19 +1,20 @@
 import express from "express"
 import mongoose, { mongo } from "mongoose"
-
+import cors from "cors"
 const app = express()
 
 // CORS middleware MUST be first
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    if (req.method === "OPTIONS") return res.sendStatus(200);
-    next();
-});
 
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     if (req.method === "OPTIONS") return res.sendStatus(200);
+//     next();
+// });
+
+app.use(cors());
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 mongoose.connect("mongodb://127.0.0.1:27017/BookInfo")
